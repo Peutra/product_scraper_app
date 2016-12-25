73f4c2c63225d5ec8c2ed9e3bf45dfdde18dd288
benoit nguyen <nguyen.hl.b@gmail.com>
2016-12-25 17:31:29 +0100

    initial commit, at this time app should be configured properly - see readme

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,7 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require bootstrap-filestyle
+//= require materialize-sprockets
+//= require turbolinks
+//= require_tree .
